130cfa47d92d2a232af43a1f2c79a110856e36f7
Adam Arling <adam.arling@northwestern.edu>
2018-04-17 00:24:22 -0500

    Jest connected component test pattern

--- src/containers/Layout.test.js
+++ src/containers/Layout.test.js
@@ -10,32 +10,34 @@ it('renders without crashing', () => {
   shallow(<Layout />);
 });
 
-it('renders Header component', () => {
-  const wrapper = shallow(<Layout />);
-  expect(wrapper.find(Header)).toHaveLength(1);
-});
+describe('Layout Component', () => {
+  let wrapper = null;
 
-it('renders Nav component', () => {
-  const wrapper = shallow(<Layout />);
-  expect(wrapper.find(Nav)).toHaveLength(1);
-});
+  beforeEach(() => {
+    wrapper = shallow(<Layout />);
+  });
 
-it('renders GlobalSearch component', () => {
-  const wrapper = shallow(<Layout />);
-  expect(wrapper.find(GlobalSearch)).toHaveLength(1);
-});
+  it('renders Header component', () => {
+    expect(wrapper.find(Header)).toHaveLength(1);
+  });
 
-it('renders the <main> html element', () => {
-  const wrapper = shallow(<Layout />);
-  expect(wrapper.find('main#main-content')).toHaveLength(1);
-});
+  it('renders Nav component', () => {
+    expect(wrapper.find(Nav)).toHaveLength(1);
+  });
 
-it('renders Switch routing component', () => {
-  const wrapper = shallow(<Layout />);
-  expect(wrapper.find(Switch)).toHaveLength(1);
-});
+  it('renders GlobalSearch component', () => {
+    expect(wrapper.find(GlobalSearch)).toHaveLength(1);
+  });
 
-it('renders Route routing components', () => {
-  const wrapper = shallow(<Layout />);
-  expect(wrapper.find(Route).length).toBeGreaterThanOrEqual(1);
+  it('renders the <main> html element', () => {
+    expect(wrapper.find('main#main-content')).toHaveLength(1);
+  });
+
+  it('renders Switch routing component', () => {
+    expect(wrapper.find(Switch)).toHaveLength(1);
+  });
+
+  it('renders Route routing components', () => {
+    expect(wrapper.find(Route).length).toBeGreaterThanOrEqual(1);
+  });
 });
