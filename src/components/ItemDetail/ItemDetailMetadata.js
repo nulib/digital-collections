2e63ce156f99679c4fd0edc86aa39eb1a97a12fe
Chris Syversen <csyversen@gmail.com>
2018-07-30 12:56:04 -0500

    good stopping/discussion point

--- src/components/ItemDetail/ItemDetailMetadata.js
+++ src/components/ItemDetail/ItemDetailMetadata.js
@@ -7,24 +7,34 @@ const ItemDetailMetadata = props => {
   }
 
   return (
-    <div aria-labelledby="tab-item-data" id="tab-panel1" role="tabpanel">
-      <LineEntry title="Title" items={props.item.title_tesim} />
-      <LineEntry title="Collections" items={props.item.admin_set_tesim} />
-      <LineEntry
-        title="Topics"
-        items={props.item.subject_topical_label_tesim}
-      />
-      <LineEntry title="Genres" items={props.item.genre_label_tesim} />
-      <LineEntry
-        title="Type of Resource"
-        items={props.item.human_readable_type_tesim}
-      />
-      <LineEntry title="Identifiers" items={props.item.ark_tesim} />
-      <LineEntry
-        title="Rights Statement"
-        items={props.item.rights_statement_tesim}
-      />
-    </div>
+    <section className="item-section contain-970 item-categories-wrapper">
+      <div id="tab-container">
+        <div aria-labelledby="tab-item-data" id="tab-panel1" role="tabpanel">
+          <LineEntry title="Title" labels={props.item.title_tesim} />
+          <LineEntry title="Collections" labels={props.item.admin_set_tesim} />
+          <LineEntry
+            title="Topics"
+            labels={props.item.subject_topical_label_tesim}
+            urls={props.item.subject_topical_tesim}
+          />
+          <LineEntry
+            title="Genres"
+            labels={props.item.genre_label_tesim}
+            urls={props.item.genre_tesim}
+          />
+          <LineEntry
+            title="Type of Resource"
+            labels={props.item.human_readable_type_tesim}
+          />
+          <LineEntry title="Identifiers" labels={props.item.ark_tesim} />
+          <LineEntry
+            title="Rights Statement"
+            labels={props.item.rights_statement_tesim}
+            urls={props.item.rights_statement_tesim}
+          />
+        </div>
+      </div>
+    </section>
   );
 };
 
--- src/components/ItemDetail/LineEntry.js
+++ src/components/ItemDetail/LineEntry.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
 const LineEntry = props => {
+  let calculatedLink = (label, i) => {
+    if (props.urls) {
+      return <a href={props.urls[i]}>{label}</a>;
+    } else {
+      return label;
+    }
+  };
+
   return (
     <div>
       <h4>{props.title}</h4>
-      <ul>{props.items.map((item, i) => <li key={i}>{item}</li>)}</ul>
+      <ul>
+        {props.labels.map((label, i) => {
+          return <li key={i}>{calculatedLink(label, i)}</li>;
+        })}
+      </ul>
     </div>
   );
 };
