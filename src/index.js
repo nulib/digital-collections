952b283e14ea887c0cdb49612cfb597433dba8b8
Michael B. Klein <mbklein@gmail.com>
2018-09-05 13:20:02 -0500

    Extract the redux store so the ElasticSearch API can access app state

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import rootReducer from './reducers';
 import Root from './Root';
-
-const store = createStore(
-  rootReducer,
-  applyMiddleware(
-    thunkMiddleware // lets us dispatch() functions
-  )
-);
+import store from './store';
 
 ReactDOM.render(<Root store={store} />, document.getElementById('root'));
 registerServiceWorker();
