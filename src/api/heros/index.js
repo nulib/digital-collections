6e139bef91ff2a0f6f60e9639a81f2ac1971d7c9
Adam Arling <adam.arling@northwestern.edu>
2018-12-03 10:26:09 -0600

    Update homepage hero section to remove Berkeley and add Fava and WPA

--- src/api/heros/index.js
+++ src/api/heros/index.js
@@ -1,8 +1,23 @@
-export const heroData = {
+export const heroBerkeley = {
   title: 'Berkeley Folk Festival',
   subTitle: 'Summer of love - collection description here',
   collectionId: 'asdf0986asdf09',
   heroImage: 'alice-at-the-greek-1440x600.png'
+};
+
+export const heroFava = {
+  title: "Commedia dell'Arte",
+  subTitle: 'The Masks of Antonio Fava',
+  collectionId: 'asdf0986asdf09',
+  heroImage: 'hero-fava-1440x600.jpg'
+};
+
+export const heroWPA = {
+  title: 'World War II Poster Collection at Northwestern University Library',
+  subTitle:
+    'View more than 300 World War II-era posters issued by various U.S. government agencies.',
+  collectionId: 'asdf0986asdf09',
+  heroImage: 'hero-wpa-1440x600.jpg'
 };
 
 export const heroSecondaryData = {
