55fa2e48e2bfd94a777c30e5ef1597306695b865
Adam Arling <adam.arling@northwestern.edu>
2018-12-06 14:48:37 -0600

    OpenSeadragon MVP, hardcoded

--- src/components/OpenDragon.js
+++ src/components/OpenDragon.js
@@ -1,41 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import OpenSeadragon from 'openseadragon';
 
 const styles = {
   viewer: {
-    width: '800px',
-    height: '600px'
+    width: '1200px',
+    height: '800px',
+    background: '#000'
   }
 };
 
-const OpenDragon = props => {
-  let viewer = OpenSeadragon({
-    id: 'openseadragon1',
-    preserveViewport: true,
-    visibilityRatio: 1,
-    minZoomLevel: 1,
-    defaultZoomLevel: 1,
-    sequenceMode: true,
-    tileSources: [
-      {
-        '@context': 'http://iiif.io/api/image/2/context.json',
-        '@id':
-          'https://libimages1.princeton.edu/loris/pudl0001%2F4609321%2Fs42%2F00000001.jp2',
-        height: 7200,
-        width: 5233,
-        profile: ['http://iiif.io/api/image/2/level2.json'],
-        protocol: 'http://iiif.io/api/image',
-        tiles: [
-          {
-            scaleFactors: [1, 2, 4, 8, 16, 32],
-            width: 1024
-          }
-        ]
-      }
-    ]
-  });
+class OpenDragon extends Component {
+  componentDidMount() {
+    OpenSeadragon({
+      id: 'openseadragon1',
+      prefixUrl: 'images/openseadragon/',
+      preserveViewport: true,
+      //showNavigationControl: false, // set this to TRUE to roll our own navigation icons
+      visibilityRatio: 1,
+      minZoomLevel: 1,
+      defaultZoomLevel: 1,
+      sequenceMode: true,
+      showReferenceStrip: true,
+      /**
+      // TODO: We'll have to look at the item manifest (ie. http://iiif.stack.rdc-staging.library.northwestern.edu/public/b1/2a/4d/b3/-c/fb/1-/43/f1/-a/36/0-/40/6f/bd/05/c2/49-manifest.json), and see if there are multiple entries in 'sequences[0].canvases'.  If so we populate the array like so, below.
+      */
+      tileSources: [
+        'http://iiif.stack.rdc-staging.library.northwestern.edu/iiif/2/b1%2F80%2F86%2F07%2F-d%2Fc6%2F6-%2F48%2Fbd%2F-a%2Fc3%2F3-%2F86%2Fca%2F9d%2F5d%2F80%2F60',
+        'http://iiif.stack.rdc-staging.library.northwestern.edu/iiif/2/5d%2F5f%2F32%2Ff0%2F-3%2Fed%2Fa-%2F46%2F77%2F-9%2F87%2Fd-%2F8e%2F7c%2F88%2Fe5%2F36%2F0b',
+        'http://iiif.stack.rdc-staging.library.northwestern.edu/iiif/2/65%2F4d%2Fc3%2Fb5%2F-a%2F6b%2F6-%2F46%2F86%2F-b%2F42%2F5-%2Fb0%2F28%2Fd8%2Ff4%2F90%2Fe2'
+      ]
+    });
+  }
 
-  return <div id="openseadragon1" style={styles.viewer} />;
-};
+  render() {
+    return (
+      <div className="landing-page">
+        <div id="page">
+          <main id="main-content" className="content" tabIndex="0">
+            <div id="openseadragon1" style={styles.viewer} />
+          </main>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default OpenDragon;
