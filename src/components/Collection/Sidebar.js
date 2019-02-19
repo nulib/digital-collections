5694bbb0c0c74475941bd165527b8fc50af2068f
Adam Arling <adam.arling@northwestern.edu>
2019-02-19 12:34:47 -0600

    Combine collection home page sidebar tabs into one

--- src/components/Collection/Sidebar.js
+++ src/components/Collection/Sidebar.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import SidebarAboutTab from './SidebarAboutTab';
 import SidebarFilterTab from './SidebarFilterTab';
+import CollectionDescription from './CollectionDescription';
+import { getESDescription } from '../../services/elasticsearch-parser';
 
 const Sidebar = props => {
-  const { collectionItems, item } = props;
+  const { item } = props;
 
   return (
     <div
@@ -15,27 +15,8 @@ const Sidebar = props => {
     >
       <div className="box">
         <div id="tab-container">
-          <Tabs selectedTabClassName="active" id="tab-container">
-            <TabList id="tabs" role="tablist">
-              <Tab>About</Tab>
-              <Tab>Filters</Tab>
-            </TabList>
-
-            <div id="tab-content">
-              {/* About tab */}
-              <TabPanel>
-                <SidebarAboutTab
-                  item={item}
-                  collectionItems={collectionItems}
-                />
-              </TabPanel>
-
-              {/* Filter */}
-              <TabPanel>
-                <SidebarFilterTab collection={item} />
-              </TabPanel>
-            </div>
-          </Tabs>
+          <CollectionDescription description={getESDescription(item)} />
+          <SidebarFilterTab collection={item} />
         </div>
       </div>
     </div>
