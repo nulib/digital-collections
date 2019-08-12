de2023345691382aaeca6a0ff72bd1727c8bbaeb
Adam Arling <adam.arling@northwestern.edu>
2019-08-12 08:39:28 -0500

    Set up @testing-library and wire up a sample test

---
+++ src/components/UI/Breadcrumbs/Breadcrumbs.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Breadcrumbs from './Breadcrumbs';
+import { renderWithRouter } from '../../../services/@testing-library-helpers';
+
+test('Breadcrumbs displays correct amount of crumbs', () => {
+  let crumbs = [
+    { title: 'Crumb1', link: '/crumb1' },
+    { title: 'Crumb2', link: '/crumb2' }
+  ];
+  const { getByTestId, debug } = renderWithRouter(
+    <Breadcrumbs items={crumbs} />
+  );
+
+  expect(getByTestId(/breadcrumbs/)).toBeInTheDocument();
+  expect(getByTestId(/breadcrumbs/).children.length).toEqual(2);
+});
+
+test('Breadcrumbs do not display if no items passed in', () => {
+  const { queryByTestId } = renderWithRouter(<Breadcrumbs />);
+  expect(queryByTestId(/breadcrumbs/)).not.toBeInTheDocument();
+});
+
+test('All breadcrumbs have links except the final breadcrumb', () => {
+  let crumbs = [
+    { title: 'Crumb1', link: '/crumb1' },
+    { title: 'Crumb2', link: '/crumb2' }
+  ];
+  const { container, getByTestId, debug } = renderWithRouter(
+    <Breadcrumbs items={crumbs} />
+  );
+  expect(container.querySelectorAll('a').length).toEqual(crumbs.length - 1);
+  expect(
+    getByTestId(/breadcrumbs/).lastChild.querySelectorAll('a').length
+  ).toEqual(0);
+});
---
+++ src/screens/ContactUs.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ScreensContactUs from './ContactUs';
+import { renderWithRouter } from '../services/@testing-library-helpers';
+
+jest.mock('../services/google-tag-manager');
+
+test('Component renders', () => {
+  const { getByTestId } = renderWithRouter(<ScreensContactUs />);
+  expect(getByTestId(/contact-us/)).toBeInTheDocument();
+});
