eaa7f55e2ee720816072755712fd306159c1b12d
Adam Arling <adam.arling@northwestern.edu>
2020-03-12 12:48:53 -0500

    Update dependencies

--- src/components/Work/DetailSummary/SocialLinks.js
+++ src/components/Work/DetailSummary/SocialLinks.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   FacebookShareButton,
   TwitterShareButton,
@@ -6,28 +6,28 @@ import {
   FacebookIcon,
   TwitterIcon,
   PinterestIcon
-} from 'react-share';
-import PropTypes from 'prop-types';
+} from "react-share";
+import PropTypes from "prop-types";
 
 const SocialLinks = props => {
   const currentLocation = String(window.location);
   const imgUrl = `${props.item.representative_file_url}/full/full/0/default.jpg`;
-  const iconBgStyleObj = { fill: '#4e2a84' };
+  const iconBgStyleObj = { fill: "#4e2a84" };
   const styles = {
     shareButton: {
-      cursor: 'pointer',
-      display: 'inline-block',
-      margin: '0 2px'
+      cursor: "pointer",
+      display: "inline-block",
+      margin: "0 2px"
     }
   };
 
   return (
     <div className="summary-list-item social-media">
       <FacebookShareButton url={currentLocation} style={styles.shareButton}>
-        <FacebookIcon size={32} round={false} iconBgStyle={iconBgStyleObj} />
+        <FacebookIcon size={32} round={false} bgStyle={iconBgStyleObj} />
       </FacebookShareButton>
       <TwitterShareButton url={currentLocation} style={styles.shareButton}>
-        <TwitterIcon size={32} round={false} iconBgStyle={iconBgStyleObj} />
+        <TwitterIcon size={32} round={false} bgStyle={iconBgStyleObj} />
       </TwitterShareButton>
       <PinterestShareButton
         url={currentLocation}
@@ -37,7 +37,7 @@ const SocialLinks = props => {
         className=""
         style={styles.shareButton}
       >
-        <PinterestIcon size={32} round={false} iconBgStyle={iconBgStyleObj} />
+        <PinterestIcon size={32} round={false} bgStyle={iconBgStyleObj} />
       </PinterestShareButton>
     </div>
   );
