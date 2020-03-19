8c390132c74276c5e09f9e84bfb9cd1fc4319c2e
Adam Arling <adam.arling@northwestern.edu>
2020-03-19 14:59:21 -0500

    Replace most class based components with functional components

--- src/screens/ContactUs.js
+++ src/screens/ContactUs.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import Breadcrumbs from "../components/UI/Breadcrumbs/Breadcrumbs";
 import { generateTitleTag } from "../services/helpers";
 import { Helmet } from "react-helmet";
@@ -17,55 +17,52 @@ const breadCrumbs = [
   }
 ];
 
-class ScreensContactUs extends Component {
-  componentDidMount() {
+const ScreensContactUs = () => {
+  useEffect(() => {
     loadDataLayer({ pageTitle: ROUTES.CONTACT.title });
-  }
+  }, []);
 
-  render() {
-    return (
-      <div className="standard-page narrow-page" data-testid="contact-us">
-        <Helmet>
-          <title>{generateTitleTag(ROUTES.CONTACT.title)}</title>
-          <script type="application/ld+json">
-            {JSON.stringify(loadDefaultStructuredData())}
-          </script>
-        </Helmet>
-        <div id="page">
-          <main id="main-content" className="content" tabIndex="0">
-            <Breadcrumbs items={breadCrumbs} />
-            <p>
-              Northwestern University Libraries' Repository and Digital Curation
-              department is located in the main library on our Evanston campus.
-              The department is responsible for digitization of library
-              collections, describing content, and creating the software that
-              runs this website.
-            </p>
+  return (
+    <div className="standard-page narrow-page" data-testid="contact-us">
+      <Helmet>
+        <title>{generateTitleTag(ROUTES.CONTACT.title)}</title>
+        <script type="application/ld+json">
+          {JSON.stringify(loadDefaultStructuredData())}
+        </script>
+      </Helmet>
+      <div id="page">
+        <main id="main-content" className="content" tabIndex="0">
+          <Breadcrumbs items={breadCrumbs} />
+          <p>
+            Northwestern University Libraries' Repository and Digital Curation
+            department is located in the main library on our Evanston campus.
+            The department is responsible for digitization of library
+            collections, describing content, and creating the software that runs
+            this website.
+          </p>
 
-            <p>
-              Questions about digitization, description of items, or software
-              can be directed here.{" "}
-              <Mailto email="repository@northwestern.edu" />
-            </p>
+          <p>
+            Questions about digitization, description of items, or software can
+            be directed here. <Mailto email="repository@northwestern.edu" />
+          </p>
 
-            <p>
-              The content on the site comes from multiple library collections,
-              departments and units. Please visit{" "}
-              <a
-                href="https://www.library.northwestern.edu/research/research-support/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Research Support
-              </a>{" "}
-              to find the contact information for the library department that
-              can best offer research assistance.
-            </p>
-          </main>
-        </div>
+          <p>
+            The content on the site comes from multiple library collections,
+            departments and units. Please visit{" "}
+            <a
+              href="https://www.library.northwestern.edu/research/research-support/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Research Support
+            </a>{" "}
+            to find the contact information for the library department that can
+            best offer research assistance.
+          </p>
+        </main>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default ScreensContactUs;
--- src/screens/Search/Search.js
+++ src/screens/Search/Search.js
@@ -1,32 +1,30 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { generateTitleTag } from "../../services/helpers";
 import { Helmet } from "react-helmet";
 import { ROUTES } from "../../services/global-vars";
 import { loadDataLayer } from "../../services/google-tag-manager";
 import Search from "../../components/Search/Search";
 
-class ScreensSearch extends Component {
-  componentDidMount() {
+const ScreensSearch = () => {
+  useEffect(() => {
     loadDataLayer({ pageTitle: ROUTES.SEARCH.title });
-  }
+  }, []);
 
-  breadcrumbs = [
+  const breadcrumbs = [
     { link: "/", title: "Home" },
     { link: "", title: "Search Results" }
   ];
 
-  render() {
-    return (
-      <div className="standard-page">
-        <Helmet>
-          <title>{generateTitleTag("Search")}</title>
-        </Helmet>
-        <div id="page" className="search">
-          <Search breadcrumbs={this.breadcrumbs} />
-        </div>
+  return (
+    <div className="standard-page">
+      <Helmet>
+        <title>{generateTitleTag("Search")}</title>
+      </Helmet>
+      <div id="page" className="search">
+        <Search breadcrumbs={breadcrumbs} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default ScreensSearch;
