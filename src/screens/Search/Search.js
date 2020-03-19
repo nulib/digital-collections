f107945291b3b50f32eb6edfd5a6a0e2ef37487c
Adam Arling <adam.arling@northwestern.edu>
2020-03-19 14:04:13 -0500

    Replace redux instances

--- src/screens/Search/Search.js
+++ src/screens/Search/Search.js
@@ -1,23 +1,28 @@
-import React, { Component } from 'react';
-import { generateTitleTag } from '../../services/helpers';
-import { Helmet } from 'react-helmet';
-import { ROUTES } from '../../services/global-vars';
-import { loadDataLayer } from '../../services/google-tag-manager';
-import Search from '../../components/Search/Search';
+import React, { Component } from "react";
+import { generateTitleTag } from "../../services/helpers";
+import { Helmet } from "react-helmet";
+import { ROUTES } from "../../services/global-vars";
+import { loadDataLayer } from "../../services/google-tag-manager";
+import Search from "../../components/Search/Search";
 
 class ScreensSearch extends Component {
   componentDidMount() {
     loadDataLayer({ pageTitle: ROUTES.SEARCH.title });
   }
 
+  breadcrumbs = [
+    { link: "/", title: "Home" },
+    { link: "", title: "Search Results" }
+  ];
+
   render() {
     return (
       <div className="standard-page">
         <Helmet>
-          <title>{generateTitleTag('Search')}</title>
+          <title>{generateTitleTag("Search")}</title>
         </Helmet>
         <div id="page" className="search">
-          <Search />
+          <Search breadcrumbs={this.breadcrumbs} />
         </div>
       </div>
     );
