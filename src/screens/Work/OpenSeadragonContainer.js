e43fd1a686c73d1bad4cbbbf7b8a69d687e27b0b
katdivyareddy10 <katdivyareddy@gmail.com>
2020-06-25 13:53:23 -0500

    Test OSD from samvera github for URL params (#557)
    * Test OSD from samvera github for URL params
    
    * Thin out OpenSeadragonContainer component to remove manifest network request
    
    Co-authored-by: Adam Arling <adam.arling@northwestern.edu>

--- src/screens/Work/OpenSeadragonContainer.js
+++ src/screens/Work/OpenSeadragonContainer.js
@@ -1,10 +1,6 @@
-import React, { Component } from "react";
-import OpenSeadragonViewer from "../../components/Work/OpenSeadragon/Viewer";
+import React from "react";
+import { OpenSeadragonViewer } from "openseadragon-react-viewer";
 import PropTypes from "prop-types";
-import { getTileSources } from "../../services/iiif-parser";
-import { getManifest } from "../../api";
-import LoadingSpinner from "../../components/UI/LoadingSpinner";
-import { getESTitle } from "../../services/elasticsearch-parser";
 
 const styles = {
   spinner: {
@@ -17,91 +13,21 @@ const styles = {
   }
 };
 
-class OpenSeadragonContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.itemTitle = getESTitle(props.item);
-  }
-
-  static propTypes = {
-    item: PropTypes.object
+export default function OpenSeadragonContainer({ item }) {
+  const options = {
+    showDropdown: true,
+    showThumbnails: true,
+    showToolbar: true,
+    deepLinking: true
   };
 
-  state = {
-    loading: true,
-    tileSources: []
-  };
-
-  componentDidMount() {
-    this.getManifest(this.props.item.iiif_manifest);
-  }
-
-  async getManifest(url) {
-    const environtmentUrl = this.getEnvironmentManifestUrl(url);
-    let response = await getManifest(environtmentUrl);
-
-    if (response.error) {
-      // TODO: Some kind of error handling to the UI here
-      return;
-    }
-    // Get the sources for OpenSeadragon viewer from the manifest
-    let tileSources = getTileSources(response);
-    this.setState({ loading: false, tileSources });
-  }
-
-  /**
-   * Helper function to update the manifest url with local dev port number ":3000".
-   * This handles 2 conditions
-   * 1.) Running the local dev environment and;
-   * 2.) Local production build, served via the "devbox.library.northwestern.edu" url
-   *
-   * This also assumes that local DONUT instance is running on port 3000.
-   */
-  getEnvironmentManifestUrl(url) {
-    if (process.env.REACT_APP_DONUT_URL.includes("staging")) {
-      const url = process.env.REACT_APP_DONUT_URL;
-      const pairtree = this.props.item.id.match(/../g).join("/");
-      return `${url}public/${pairtree}-manifest.json`;
-    }
-
-    if (process.env.REACT_APP_LIVE_IIIF === "true") {
-      return url;
-    }
-
-    if (
-      process.env.NODE_ENV === "development" ||
-      url.indexOf("http://devbox.library.northwestern.edu") > -1
-    ) {
-      const publicIndex = url.indexOf("/public");
-      return (
-        url.slice(0, publicIndex) + ":3000" + url.slice(publicIndex, url.length)
-      );
-    }
-    return url;
-  }
-
-  render() {
-    const { loading, tileSources } = this.state;
-    const { item } = this.props;
-    const rightsStatement = item ? item.rights_statement : null;
-    const fileUrl = item ? item.representative_file_url : null;
-
-    return (
-      <div>
-        <LoadingSpinner loading={loading} />
-        {tileSources.length > 0 && (
-          <section style={styles.wrapper} data-testid="section-open-seadragon">
-            <OpenSeadragonViewer
-              tileSources={tileSources}
-              itemTitle={this.itemTitle}
-              rightsStatement={rightsStatement}
-              fileUrl={fileUrl}
-            />
-          </section>
-        )}
-      </div>
-    );
-  }
+  return (
+    <section style={styles.wrapper} data-testid="section-open-seadragon">
+      <OpenSeadragonViewer manifestUrl={item.iiif_manifest} options={options} />
+    </section>
+  );
 }
 
-export default OpenSeadragonContainer;
+OpenSeadragonContainer.propTypes = {
+  item: PropTypes.object
+};
