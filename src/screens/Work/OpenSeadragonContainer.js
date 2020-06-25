f31cd2a826854cee86dce9087e4ed401f693d256
Adam Arling <adam.arling@northwestern.edu>
2020-06-25 11:40:16 -0500

    Thin out OpenSeadragonContainer component to remove manifest network request

--- src/screens/Work/OpenSeadragonContainer.js
+++ src/screens/Work/OpenSeadragonContainer.js
@@ -1,9 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
 import { OpenSeadragonViewer } from "openseadragon-react-viewer";
 import PropTypes from "prop-types";
-import { getManifest } from "../../api";
-import LoadingSpinner from "../../components/UI/LoadingSpinner";
-import { getESTitle } from "../../services/elasticsearch-parser";
 
 const styles = {
   spinner: {
@@ -16,89 +13,21 @@ const styles = {
   }
 };
 
-class OpenSeadragonContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.itemTitle = getESTitle(props.item);
-  }
-
-  static propTypes = {
-    item: PropTypes.object
+export default function OpenSeadragonContainer({ item }) {
+  const options = {
+    showDropdown: true,
+    showThumbnails: true,
+    showToolbar: true,
+    deepLinking: true
   };
 
-  state = {
-    loading: true,
-    manifestUrl: ""
-  };
-
-  componentDidMount() {
-    this.getManifest(this.props.item.iiif_manifest);
-  }
-
-  async getManifest(url) {
-    const environtmentUrl = this.getEnvironmentManifestUrl(url);
-    let response = await getManifest(environtmentUrl);
-
-    if (response.error) {
-      // TODO: Some kind of error handling to the UI here
-      return;
-    }
-    // Get the sources for OpenSeadragon viewer from the manifest
-    this.setState({ loading: false, manifestUrl: environtmentUrl });
-  }
-
-  /**
-   * Helper function to update the manifest url with local dev port number ":3000".
-   * This handles 2 conditions
-   * 1.) Running the local dev environment and;
-   * 2.) Local production build, served via the "devbox.library.northwestern.edu" url
-   *
-   * This also assumes that local DONUT instance is running on port 3000.
-   */
-  getEnvironmentManifestUrl(url) {
-    if (process.env.REACT_APP_DONUT_URL.includes("staging")) {
-      const url = process.env.REACT_APP_DONUT_URL;
-      const pairtree = this.props.item.id.match(/../g).join("/");
-      return `${url}public/${pairtree}-manifest.json`;
-    }
-
-    if (process.env.REACT_APP_LIVE_IIIF === "true") {
-      return url;
-    }
-
-    if (
-      process.env.NODE_ENV === "development" ||
-      url.indexOf("http://devbox.library.northwestern.edu") > -1
-    ) {
-      const publicIndex = url.indexOf("/public");
-      return (
-        url.slice(0, publicIndex) + ":3000" + url.slice(publicIndex, url.length)
-      );
-    }
-    return url;
-  }
-
-  render() {
-    const { loading, manifestUrl } = this.state;
-
-    const options = {
-      showDropdown: true,
-      showThumbnails: true,
-      showToolbar: true,
-      deepLinking: true
-    };
-
-    return (
-      <div>
-        <LoadingSpinner loading={loading} />
-        {manifestUrl && (
-          <section style={styles.wrapper} data-testid="section-open-seadragon">
-            <OpenSeadragonViewer manifestUrl={manifestUrl} options={options} />
-          </section>
-        )}
-      </div>
-    );
-  }
+  return (
+    <section style={styles.wrapper} data-testid="section-open-seadragon">
+      <OpenSeadragonViewer manifestUrl={item.iiif_manifest} options={options} />
+    </section>
+  );
 }
 
-export default OpenSeadragonContainer;
+OpenSeadragonContainer.propTypes = {
+  item: PropTypes.object
+};
