bf9b13bc7fd1209f641b34080f30314bff0a92be
Michael B. Klein <mbklein@gmail.com>
2021-03-18 12:30:15 -0500

    Force DC to redirect to HTTPS

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,7 +88,7 @@ resource "aws_cloudfront_distribution" "fen" {
       }
     }
 
-    viewer_protocol_policy = "allow-all"
+    viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
