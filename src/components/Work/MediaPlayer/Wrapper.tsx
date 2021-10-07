2386a1f1d120da398b98c782eadb548524a0c345
Adam J. Arling <aarling@gmail.com>
2021-10-07 14:50:17 -0500

    Support react media player on Work screen

---
+++ src/components/Work/MediaPlayer/Wrapper.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactMediaPlayer from "@nulib/react-media-player";
+
+// This is for testing
+const url =
+  "https://raw.githubusercontent.com/mathewjordan/mirador-playground/main/assets/iiif/manifest/assortedCanvases.json";
+
+export interface WorkMediaPlayerWrapperProps {
+  manifestId: string;
+}
+
+const WorkMediaPlayerWrapper: React.FC<WorkMediaPlayerWrapperProps> = ({
+  manifestId,
+}) => {
+  if (!manifestId) return null;
+  return (
+    <div data-testid="media-player-wrapper">
+      <ReactMediaPlayer manifestId={manifestId} />
+    </div>
+  );
+};
+
+export default WorkMediaPlayerWrapper;
