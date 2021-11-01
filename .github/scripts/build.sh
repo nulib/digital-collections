118d508fbd5b2b8e12cdc98a73fbe3dca57f60ad
Michael B. Klein <mbklein@gmail.com>
2021-11-01 12:31:55 -0500

    Switch build from CircleCI to Github Actions

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -6,10 +6,13 @@ export REACT_APP_IIIF_LOGIN_URL=$(aws ssm get-parameter --name /stack-glaze/reac
 export REACT_APP_SHARED_ITEM_PROXY_URL=$(aws ssm get-parameter --name /stack-glaze/react_app_shared_item_proxy_url | jq -r '.Parameter.Value')
 S3_BUCKET=$(aws ssm get-parameter --name /dc/s3_bucket | jq -r '.Parameter.Value')
 export REACT_APP_WORK_ARCHIVER_ENDPOINT=$(aws ssm get-parameter --name /stack-glaze/react_app_work_archiver_endpoint | jq -r '.Parameter.Value')
-
+export FEN_DISTRIBUTION_ID=$(aws ssm get-parameter --name /stack-glaze/distribution_id | jq -r '.Parameter.Value')
 
 echo "REACT_APP_ELASTICSEARCH_PROXY_BASE=$REACT_APP_ELASTICSEARCH_PROXY_BASE REACT_APP_DONUT_URL=$REACT_APP_DONUT_URL REACT_APP_IIIF_LOGIN_URL=$REACT_APP_IIIF_LOGIN_URL REACT_APP_SHARED_ITEM_PROXY_URL=$REACT_APP_SHARED_ITEM_PROXY_URL S3_BUCKET=$S3_BUCKET"
 
 npm install
 npm run-script build
 aws s3 sync --delete --exclude '*.xml.gz' --acl public-read ./build/ s3://${S3_BUCKET}/
+
+echo "Invalidating CloudFront Cache"
+aws cloudfront create-invalidation --distribution-id ${FEN_DISTRIBUTION_ID} --invalidation-batch "Paths={Quantity=1,Items=[/*]},CallerReference=$(date +%Y%m%d%H%M%S)"
