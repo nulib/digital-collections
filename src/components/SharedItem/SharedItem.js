558826f3f9a9d2d502cb9660f70c29b9c7848ccf
Mat Jordan <mat@northwestern.edu>
2022-02-01 09:27:02 -0500

    Add work type conditional IIIF viewer for shared items.

--- src/components/SharedItem/SharedItem.js
+++ src/components/SharedItem/SharedItem.js
@@ -1,15 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
-import OpenSeadragonContainer from "../../screens/Work/OpenSeadragonContainer";
-import Work from "../Work/Work";
-
+import OpenSeadragonContainer from "screens/Work/OpenSeadragonContainer";
+import useWorkType from "hooks/use-work-type";
+import WorkMediaPlayerWrapper from "components/Work/MediaPlayer/Wrapper";
+import Work from "components/Work/Work";
 function SharedItem({ work }) {
-  if (!work) {
-    return null;
-  }
+  const { isMediaType } = useWorkType();
+
+  if (!work) return null;
+
   return (
     <div data-testid="shared-item">
-      <OpenSeadragonContainer item={work} />
+      {work && !isMediaType(work.workType) && (
+        <OpenSeadragonContainer item={work} />
+      )}
+      {work && isMediaType(work.workType) && (
+        <WorkMediaPlayerWrapper manifestId={work.iiifManifest} />
+      )}
+
       <div id="page">
         <main id="main-content" className="content" tabIndex="0">
           <Work work={work} />
