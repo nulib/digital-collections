5ba1c42d520f257dab1d56fe26242f56bc528c49
Karen Shaw <karendid@gmail.com>
2022-02-03 14:51:12 -0700

    Change wording of notification on shared link page

--- src/components/SharedItem/Notification.js
+++ src/components/SharedItem/Notification.js
@@ -5,10 +5,25 @@ import moment from "moment";
 function SharedItemNotification({ expirationDate }) {
   return (
     <div className="shared-link-notification">
-      This work was shared with you via a temporary link. The link will expire:{" "}
-      <strong>
-        {moment(expirationDate).format("MMMM Do YYYY, h:mm:ss a")}
-      </strong>
+      <p>
+        Access to this resource is for educational, personal and non commercial
+        use. Written permission of copyright holders is required for
+        distribution.{" "}
+        <a
+          href="https://www.library.northwestern.edu/about/administration/policies/rights-permissions.html"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Read more
+        </a>
+      </p>
+      <p>
+        This work was shared with you via a temporary link. The link will
+        expire:{" "}
+        <strong>
+          {moment(expirationDate).format("MMMM Do YYYY, h:mm:ss a")}
+        </strong>
+      </p>
     </div>
   );
 }
